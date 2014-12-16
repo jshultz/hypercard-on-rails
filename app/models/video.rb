ee096a5f31b58454622ca552fbaa0b6da80f6c36
Jason Shultz <jasshultz@gmail.com>
2014-12-16 07:28:06 -0700

    obfuscating file paths for the greater good

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,8 +2,8 @@ class Video < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :profilevideo,
-                    :url => "/videos/:attachment/:id_:basename.:extension",
-                    :path => ":rails_root/public/videos/:attachment/:id_:basename.:extension",
+                    :url => "/videos/:attachment/:hash/:id_:basename.:extension",
+                    :path => ":rails_root/public/videos/:attachment/:hash/:id_:basename.:extension",
   :processors => [:transcoder]
 
   validates_attachment_content_type :profilevideo, :content_type => ["video/mp4", "video.mov", "video/mpeg","video/mpeg4", "image/jpg", "image/jpeg"]
---
+++ config/initializers/paperclip_defaults.rb
@@ -0,0 +1,4 @@
+Paperclip::Attachment.default_options.update({
+   :path => ":class/:attachment/:hash/:style.:extension",
+   :hash_secret => 'MftemxBP53saJaYLL3G66V42'
+})
