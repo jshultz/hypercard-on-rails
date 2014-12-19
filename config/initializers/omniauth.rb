8ab001f57977f082f563f4d369208a8c914c7d8b
Jason Shultz <jasshultz@gmail.com>
2014-12-19 08:39:05 -0700

    ominauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Rails.application.secrets.facebook_provider_key, Rails.application.secrets.facebook_provider_secret
-  provider :twitter, Rails.application.secrets.twitter_provider_key, Rails.application.secrets.twitter_provider_secret
+  provider :twitter, Settings.twitter.consumer_key, Settings.twitter.consumer_secret
 
 end
